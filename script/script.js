6b6a91b018968771c719cfc60cf1eb38a2d2c7e5
MXZART <mxzart@localhost.localdomain>
2022-05-22 01:30:53 +0800

    Portfolio Ver 0.1

---
+++ script/script.js
@@ -0,0 +1,7 @@
+/* ========================= Typing Animation ========================= */
+var typed = new Typed(".typing", {
+    strings:["","Web Designer","Web Developer","Security Analyst","Data Analyst"],
+    typeSpeed:100,
+    BackSpeed:70,
+    loop:true
+})
